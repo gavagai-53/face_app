a1f116260cd52e1c97d03543818131305bfa405f
jschn <jan_schneider@live.de>
2018-01-10 12:09:28 +0100

    fixed model loading

--- app.R
+++ app.R
@@ -1,18 +1,6 @@
 library(shiny)
 source("face_functions.R")
 
-#TODO: solve model loading more elegantly
-
-load("models/pls_res_asd.Rdat")
-pls_res_asd=pls_res
-rm(pls_res)
-load("models/pls_res_NTASDvids.Rdat")
-
-# load('models/pls_res_asd.Rdat')
-# pls_res_asd=pls_res
-# rm(pls_res)
-# load('models/pls_res_NTASDvids.Rdat')
-
 connections <- read.csv('adj_face_points.csv', header = FALSE)
 
 # define time range globally 
@@ -83,7 +71,10 @@ ui <- fluidPage(
       
 
       
-      actionButton("play", "Play!")
+      actionButton("play", "Play!"),
+      actionButton("loadmodel1","Model1"),
+      actionButton("loadmodel2","Model2")
+      
       
     ),
     
@@ -100,6 +91,17 @@ ui <- fluidPage(
 # Define server logic required to draw the plots
 server <- function(input, output, session) {
 
+  #load two default models here
+  #model1
+  object_name=load("models/pls_res_NTASDvids.Rdat")
+  pls_res1=get(object_name)
+  rm(object_name)
+  #model2
+  object_name=load("models/pls_res_asd.Rdat")
+  pls_res2=get(object_name)
+  rm(object_name)
+
+  
   state <- reactiveValues()
   state$running <- FALSE
   
@@ -107,8 +109,22 @@ server <- function(input, output, session) {
     state$running <- TRUE
   })
   
+  observeEvent(input$loadmodel1, {
+    chosenfile=file.choose()
+    object_name=load(chosenfile)
+    pls_res1<<-get(object_name)
+    rm(object_name)
+  })
+  
+  observeEvent(input$loadmodel2, {
+    chosenfile=file.choose()
+    object_name=load(chosenfile)
+    pls_res2<<-get(object_name)
+    rm(pls_res)
+  })
+  
   output$distPlot <- renderPlot({
-    
+
     if ((input$time < time_range[2]) && state$running){
       # raise time state
       updateSliderInput(
@@ -144,7 +160,7 @@ server <- function(input, output, session) {
       input$interested
     )
 
-    plot_same_window(new_vec,pls_res,pls_res_asd,input$time)
+    plot_same_window(new_vec,pls_res1,pls_res2,input$time)
     output$labels=renderText(paste(find_labels(new_vec,70)," ",collapse=''))
     
   }, height=1000, width=1000
